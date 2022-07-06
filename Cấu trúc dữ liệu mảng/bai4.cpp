86855ebc854f35fec351e90c409ef868b3295cc4
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-07-06 10:42:09 +0700

    C++ 06/07/2022

--- Cấu trúc dữ liệu mảng/bai4.cpp
+++ Cấu trúc dữ liệu mảng/bai4.cpp
@@ -62,7 +62,7 @@ int main() {
     } while(k > n);
 
     int *ptr = insert_Array(n, arr, k, x);
-    for(int i=0; i<n + 1; i++) {
+    for(int i = 0; i < n + 1; i++) {
         cout << *(ptr + i) << " ";
     }
 
